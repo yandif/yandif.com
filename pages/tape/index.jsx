02abb8260d290a10b9d896296852835f5888a3f7
yandif <1322278095@qq.com>
2021-03-10 21:53:31 +0800

    fix:修改Tape的title

--- pages/tape/index.jsx
+++ pages/tape/index.jsx
@@ -1,2 +1,20 @@
 import Tape from "../../components/tape/index";
-export default Tape;
+import Head from "next/head";
+
+export default function () {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Tape" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1, maximum-scale=1,user-scalable=no"
+        />
+
+        <title>Tape</title>
+      </Head>
+      <Tape></Tape>
+    </>
+  );
+}
