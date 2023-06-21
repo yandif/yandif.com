587f524d000a40fd1f3571e9fb7e0edc21c4b671
yandif <me.yandif@gmail.com>
2023-06-21 17:34:03 +0800

    feat: 添加 命名格式转换工具

---
+++ components/tool/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { camelCase, lowerCase, snakeCase, startCase, upperCase } from 'lodash';
+
+export function Tool() {
+  const [text, setText] = useState();
+  return (
+    <div>
+      <div>
+        输入：
+        <input
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          type="text"
+        />{' '}
+      </div>
+      <div>驼峰命名：{camelCase(text)} </div>
+      <div>蛇形命名：{snakeCase(text)} </div>
+      <div>首字母大写：{startCase(text)} </div>
+      <div>小写：{lowerCase(text)} </div>
+      <div>大写：{upperCase(text)} </div>
+    </div>
+  );
+}
